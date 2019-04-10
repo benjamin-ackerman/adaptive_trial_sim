c2ed314ac1d9b359a77f385a4d183f5f504a3a2b
Benjamin Ackerman <backer10@jhu.edu>
2019-04-09 21:03:45 -0400

    updated gather_results script to add simulation number to order the results

--- gather_results.R
+++ gather_results.R
@@ -2,5 +2,6 @@ library(stringr);library(dplyr);library(purrr)
 results = list.files()[str_detect(list.files(),".rds")]
 
 seq_along(results) %>% 
-  map_df(~readRDS(results[.])) %>% 
+  map_df(function(x){readRDS(results[x]) %>% 
+           mutate(sim_num = str_extract(results[x],"[1-9]"))}) %>% 
   saveRDS(.,"combined_results.rds")
